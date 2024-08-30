e05a0e485d669e332a057f4edbf1bbbdc194d74b
Paco Lovera <pacolovera29@gmail.com>
2024-08-29 22:27:07 -0300

    Agregando excepción por division 0

--- src/calculator.py
+++ src/calculator.py
@@ -5,4 +5,6 @@ def calculate(a,b, operator):
         raise ValueError(f"Operador no valido: {operator}")
 
 def division(a,b):
+    if b == 0:
+        raise ValueError("No puedes dividir por 0")
     return a / b
