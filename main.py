2a3908af8badec140e21eb88ad3a1427b54571e5
MATIAS DURAN <matiduran03@gmail.com~>
2024-08-30 11:55:39 -0300

    agrego try

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from src import calculator
 
 def main():
-    num1= float(input("Enter first number: "))
-    num2= float(input("Enter second number: "))
-    operator= input("Enter operator: ")
-    result= calculator.calculate(num1, num2, operator)
-    print(f"Result: {result}")
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+        operator = input("Enter operator: ")
+
+        result = calculator.calculate(num1, num2, operator)
+        print(f"Result: {result}")
+
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     main()
-    
