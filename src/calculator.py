50eaf3c84542c51453ba3ae1d6436ee900f466b2
Lautaro Francisco Bleiz <lautaro.bleiz77@gmail.com>
2024-08-30 10:56:52 -0300

    Agregación de la funcionalidad resta

--- src/calculator.py
+++ src/calculator.py
@@ -3,7 +3,9 @@ def calculate(a,b, operator):
         return multiplicacion(a,b)
     elif operator == "/":
         return division(a,b)
-    else: 
+    elif operator == "-": 
+        return resta(a,b)
+    else:
         raise ValueError(f"Operador no valido: {operator}")
 
 def multiplicacion(a,b):
@@ -13,3 +15,6 @@ def division(a,b):
     if b == 0:
         raise ValueError("No puedes dividir por 0")
     return a / b
+
+def resta(a,b):
+    return a - b
