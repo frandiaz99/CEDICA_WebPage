e3299473acecb986f6c63f2ec4db4bf02e719f68
Francisco Diaz Malbran <fdmalbran@alu.ing.unlp.edu.ar>
2024-09-16 14:48:12 -0300

    test y cambios de estilo

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./admin/src/web/templates/**/*.{html,js}', // Para los archivos HTML en templates
+    './admin/static/**/*.{css}'],               // Para los archivos CSS en static],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
