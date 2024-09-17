21587792b704c6a625760d2c257e92d22ac3e971
Francisco Diaz Malbran <fdmalbran@alu.ing.unlp.edu.ar>
2024-09-17 18:17:14 -0300

    nombre tabla

--- admin/src/core/board/issue.py
+++ admin/src/core/board/issue.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from src.core.database import db
 
 class Issue(db.Model):
-    
+    __tablename__ = "issues"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), nullable=False)
     title = db.Column(db.String(100), nullable=False)
