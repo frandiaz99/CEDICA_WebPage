58dc936ba941b52e121420ab66a8a5f559dda6bb
MATIAS DURAN <matiduran03@gmail.com~>
2024-09-18 15:30:32 -0300

    labels

--- admin/src/core/board/__init__.py
+++ admin/src/core/board/__init__.py
@@ -1,5 +1,6 @@
 from src.core.board.issue import Issue
 from src.core.database import db
+from src.core.board.label import Label
 
 def list_issues():
     issues = Issue.query.all()
@@ -15,6 +16,20 @@ def create_issue(**kwargs):
 
 def assign_user(issue, user):
     issue.user = user
+    db.session.add(issue)
+    db.session.commit()
+
+    return issue
+
+def create_label(**kwargs):
+    label = Label(**kwargs)
+    db.session.add(label)
+    db.session.commit()
+
+    return label
+
+def assign_labels(issue, labels):
+    issue.labels = labels
     db.session.add(issue)
     db.session.commit()
 
--- admin/src/core/seeds.py
+++ admin/src/core/seeds.py
@@ -1,4 +1,4 @@
-from src.core import board
+from src.core import board, auth
 
 def run():
     issue1 = board.create_issue(
@@ -20,4 +20,19 @@ def run():
         description="No anda la impresora",
         status="done"
     )
-    
+    fede = auth.create_user(email="fede@gmail.com", password="1234")    
+    mati = auth.create_user(email="mati@gmail.com", password="1234")    
+    miguel = auth.create_user(email="miguel@gmail.com", password="1234")        
+
+    board.assign_user(issue1, fede)
+    board.assign_user(issue2, mati)
+    board.assign_user(issue3, miguel)
+
+    label1 = board.create_label(title="Urgente",description="Issues que tienen que resolverse dentro de 24hs",)
+    label2 = board.create_label(title="Importante",description="Issues de alta prioridad",)
+    label3 = board.create_label(title="Soporte",description="Issues relacionados con soporte técnico",)
+    label4 = board.create_label(title="Ventas",description="Issues relacionados con el área de ventas",)
+
+    board.assign_labels(issue1, [label1, label2])
+    board.assign_labels(issue2, [label3, label4])
+    board.assign_labels(issue3, [label1, label3])
