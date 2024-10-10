65d36e60ca050e54356f737b77398577a003ca2f
MATIAS DURAN <matiduran03@gmail.com>
2024-10-10 00:29:27 -0300

    subir documentos

--- admin/src/web/controllers/equipo.py
+++ admin/src/web/controllers/equipo.py
@@ -1,18 +1,18 @@
-from flask import Blueprint, render_template, request, abort, flash, url_for, redirect
+from flask import Blueprint, render_template, request, abort, flash, url_for, redirect, current_app
 from sqlalchemy import asc, desc
-from src.core import equipo
-
-
+from src.core.database import db
+from datetime import datetime
 equipo_bp = Blueprint('equipo', __name__, url_prefix='/equipo')
 
 from flask import Blueprint, render_template, request
 from sqlalchemy import asc, desc
-from src.core.equipo import Empleado
-
-
+from src.core.equipo import Empleado, documento
+from src.web.handlers.auth import check
+import os 
 equipo_bp = Blueprint('equipo', __name__, url_prefix='/equipo')
 
 @equipo_bp.get("/")
+@check('equipo_index')
 def index():
     registros_por_pagina = 5
     # Obtener parámetros de búsqueda y orden desde la URL
@@ -53,7 +53,6 @@ def index():
         query = query.order_by(asc(Empleado.inserted_at)) if order == 'asc' else query.order_by(desc(Empleado.inserted_at))
 
     pagination = query.paginate(page=pagina, per_page=registros_por_pagina)
-    
     empleados = pagination.items
 
     total_paginas = pagination.pages
@@ -76,7 +75,31 @@ def detalle_empleado(id):
     empleado = Empleado.query.get(id)
     if empleado is None:
         abort(404)  # error 404 si no se encuentra el empleado
-    return render_template('equipo/detalle_empleado.html', empleado=empleado)
+    
+    registros_por_pagina = 5
+    query = documento.Documento.query
+    pagina = request.args.get('pagina', 1, type=int)
+    pagination = query.paginate(page=pagina, per_page=registros_por_pagina)
+    documentos = pagination.items
+    total_paginas = pagination.pages
+    
+    return render_template('equipo/detalle_empleado.html', empleado=empleado, documentos=documentos, pagina=pagina, total_paginas=total_paginas)
+
+@equipo_bp.route('/eliminar/<int:id>', methods=['POST'])
+@check("equipo_destroy")
+def eliminar_empleado(id):
+    # Buscar el encuestre por ID
+    equipo_aux = Empleado.query.get_or_404(id)
+    
+    try:
+        db.session.delete(equipo_aux)
+        db.session.commit()
+
+        flash('Equipo eliminado correctamente.', 'success')
+    except Exception as e:
+        db.session.rollback()
+        flash(f'Error al eliminar el equipo: {str(e)}', 'danger')
+    return redirect(url_for('equipo.index')) 
 
 
 @equipo_bp.route('/registrar', methods=['GET', 'POST'])
@@ -116,3 +139,96 @@ def registrar_empleado():
             return redirect(url_for('equipo.registrar_empleado'))
 
     return render_template('equipo/registrar_empleado.html')
+
+@equipo_bp.route('/editar/<int:id>', methods=['GET', 'POST'])
+# @check("empleado_update")
+def editar_empleado(id):
+    empleado_aux = Empleado.query.get(id)
+    if empleado_aux is None:
+        abort(404) 
+
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        empleado_aux.nombre = request.form['nombre']
+        empleado_aux.apellido = request.form['apellido']
+        empleado_aux.dni = request.form['dni']
+        empleado_aux.domicilio = request.form['domicilio']
+        empleado_aux.email = request.form['email']
+        empleado_aux.localidad = request.form['localidad']
+        empleado_aux.telefono = request.form['telefono']
+        empleado_aux.profesion = request.form['profesion']
+        empleado_aux.puesto_laboral = request.form['puesto_laboral']
+        empleado_aux.fecha_inicio = request.form['fecha_inicio']
+        empleado_aux.fecha_cese = request.form.get('fecha_cese')
+        empleado_aux.contacto_emergencia = request.form['contacto_emergencia']
+        empleado_aux.obra_social = request.form['obra_social']
+        empleado_aux.numero_afiliado = request.form['numero_afiliado']
+        empleado_aux.condicion = request.form['condicion']
+        empleado_aux.activo = True if request.form['activo'] == 'si' else False
+
+        # Guardar cambios en la base de datos
+        try:
+            db.session.commit()
+            flash('Los cambios se han guardado exitosamente.', 'success')
+            return redirect(url_for('equipo.detalle_empleado', id=empleado_aux.id))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error al actualizar el empleado: {str(e)}', 'danger')
+            return redirect(url_for('equipo.editar_empleado', id=id))
+    
+    return render_template(
+        'equipo/editar_empleado.html', 
+        empleado=empleado_aux, 
+        fecha_hoy=datetime.today().date()
+    )
+
+@equipo_bp.route('/subir_documento', methods=['POST'])
+def subir_documento():
+    MAX_FILE_SIZE = 16 * 1024 * 1024
+
+    if 'file' not in request.files:
+        flash('No se seleccionó ningún archivo', 'error')
+        return redirect(url_for('equipo.detalle_empleado', id=request.form.get('empleado_id')))
+    
+    file = request.files['file']
+
+    client = current_app.storage.client 
+
+    file_size = os.fstat(file.fileno()).st_size
+    
+    if file_size > MAX_FILE_SIZE:
+        flash('El archivo excede el tamaño máximo permitido (16MB)', 'error')
+        return redirect(url_for('equipo.detalle_empleado', id=request.form.get('empleado_id')))
+    
+    if file.filename == '':
+        flash('Nombre de archivo vacío', 'error')
+        return redirect(url_for('equipo.detalle_empleado', id=request.form.get('empleado_id')))
+    
+    try:
+        client.put_object(
+            'grupo49',
+            file.filename,
+            file,
+            file_size, 
+            content_type=file.content_type
+        )
+        
+        # Obtener el empleado asociado
+        empleado_id = request.form.get('empleado_id')
+        empleado_aux = Empleado.query.get(empleado_id)
+        
+        # Crear el documento y asociarlo al empleado
+        nuevo_documento = documento.Documento(
+            titulo=file.filename,
+            url=f"{current_app.config['MINIO_SERVER']}/grupo49/{file.name}",
+            empleado=empleado_aux
+        )
+        
+        db.session.add(nuevo_documento)
+        db.session.commit()
+        
+        flash('Documento subido exitosamente', 'success')
+    except Exception as e:
+        flash(f'Error al subir el documento: {str(e)}', 'error')
+    
+    return redirect(url_for('equipo.detalle_empleado', id=request.form.get('empleado_id')))
