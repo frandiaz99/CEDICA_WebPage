48c5150486f6e0fd1e56abbbc587b4a2b2502025
Francisco Diaz Malbran <fdmalbran@alu.ing.unlp.edu.ar>
2024-10-15 15:40:39 -0300

    actualizacion docstrings

--- admin/src/core/rol_permiso/permisos_roles.py
+++ admin/src/core/rol_permiso/permisos_roles.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from src.core.database import db
 
-# Tabla intermedia para la relación muchos a muchos
 permisos_roles = db.Table('permisos_roles',
     db.Column('rol_id', db.Integer, db.ForeignKey('roles.id'), primary_key=True),
     db.Column('permiso_id', db.Integer, db.ForeignKey('permisos.id'), primary_key=True),
