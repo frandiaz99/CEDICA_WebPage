ccf06d3001aeaad87d6d321237416239ad9e0816
Francisco Diaz Malbran <fdmalbran@alu.ing.unlp.edu.ar>
2024-10-30 13:18:34 -0300

    app publica

---
+++ web/src/components/Noticias.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="noticias container mx-auto p-8">
+      <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Noticias</h2>
+      <ul>
+        <li v-for="noticia in noticias" :key="noticia.id" class="bg-gray-100 p-6 rounded-lg mb-4 shadow-md">
+          <h3 class="text-xl font-semibold text-gray-800">{{ noticia.titulo }}</h3>
+          <p class="text-gray-500 text-sm">{{ noticia.fechaPublicacion }}</p>
+          <p class="text-gray-700">{{ noticia.copete }}</p>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NoticiasView',
+    data() {
+      return {
+        noticias: [
+          { id: 1, titulo: "Noticia 1", fechaPublicacion: "2023-10-01", copete: "Resumen de la noticia 1" },
+          { id: 2, titulo: "Noticia 2", fechaPublicacion: "2023-09-20", copete: "Resumen de la noticia 2" },
+        ],
+      };
+    },
+    created() {
+      // Cargar noticias desde la API aquí
+    },
+  };
+  </script>
+  
---
+++ web/src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../components/Home.vue';
+import ContactoView from '../components/Contacto.vue'; 
+import NoticiasView from '../components/Noticias.vue'; 
+import MainLayout from '../views/MainLayout.vue';
+
+const routes = [
+    
+    { path: '/', name: 'Home', component: MainLayout},
+    { path: '/home', name: 'Home', component: HomeView },
+    { path: '/contacto', name: 'Contacto', component: ContactoView},
+    { path: '/noticias', name: 'Noticias', component: NoticiasView },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
