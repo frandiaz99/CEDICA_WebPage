2921ad685ce25afb98b6724cc2186cb976c1fd7d
Lautaro Francisco Bleiz <lautaro.bleiz77@gmail.com>
2024-11-20 09:29:42 -0300

    Visualizacion de noticias en portal. Falta link y privada

--- portal/src/components/Noticias.vue
+++ portal/src/components/Noticias.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class="noticias container mx-auto p-8">
-      <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Noticias</h2>
-      <ul>
-        <li v-for="noticia in noticias" :key="noticia.id" class="bg-gray-100 p-6 rounded-lg mb-4 shadow-md">
-          <h3 class="text-xl font-semibold text-gray-800">{{ noticia.titulo }}</h3>
-          <p class="text-gray-500 text-sm">{{ noticia.fechaPublicacion }}</p>
-          <p class="text-gray-700">{{ noticia.copete }}</p>
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'NoticiasView',
-    data() {
-      return {
-        noticias: [
-          { id: 1, titulo: "Noticia 1", fechaPublicacion: "2023-10-01", copete: "Resumen de la noticia 1" },
-          { id: 2, titulo: "Noticia 2", fechaPublicacion: "2023-09-20", copete: "Resumen de la noticia 2" },
-        ],
-      };
-    },
-    created() {
-      // Cargar noticias desde la API aquí
-      
-    },
-  };
-  </script>
-  
+  <div class="noticias container mx-auto p-8">
+    <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Noticias</h2>
+    <ul>
+      <li v-for="noticia in noticias" :key="noticia.id" class="bg-gray-100 p-6 rounded-lg mb-4 shadow-md">
+
+        <p class="text-xl font-semibold text-gray-800 w-full text-center">{{ noticia.titulo }}</p>
+        <p class="text-gray-500 text-sm">Subido por: {{ noticia.autor }}</p>
+        <p class="text-gray-400 text-sm">{{ noticia.fecha_publicacion }}</p>
+        <p class="text-gray-700">{{ noticia.copete }}</p>
+
+        <a href="">Ver noticia completa</a>
+
+      </li>
+    </ul>
+    <p v-if="!noticias.length">No hay noticias nuevas.</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'NoticiasView',
+  data() {
+    return {
+      noticias: [],
+      error: null, // Para manejar posibles errores
+    };
+  },
+  async created() {
+    try {
+      // Hacer la solicitud GET usando async/await
+      const response = await axios.get('http://127.0.0.1:5000/api/publicaciones');
+      this.noticias = response.data.map(noticia => ({
+        ...noticia,
+      }));
+    } catch (error) {
+      console.error('Hubo un error al cargar las noticias:', error);
+      this.error = 'Hubo un problema al cargar las noticias.';
+    }
+  },
+};
+</script>
