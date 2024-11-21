ff63f62e1f8577f38bcebf5f2a2461f3d575203b
MATIAS DURAN <matiduran03@gmail.com>
2024-11-21 18:13:59 -0300

    cambio url

---
+++ portal/src/components/Contacto.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="contact container mx-auto p-8">
+    <h2 class="text-2xl font-bold text-center text-gray-800 mb-6">Contacto</h2>
+    <form @submit.prevent="submitForm" class="bg-gray-100 p-6 rounded-lg shadow-md max-w-md mx-auto">
+      <div class="mb-4">
+        <label for="name" class="block text-gray-700">Nombre completo</label>
+        <input type="text" id="name" v-model="name" required class="w-full p-2 border border-gray-300 rounded mt-1" />
+      </div>
+      <div class="mb-4">
+        <label for="email" class="block text-gray-700">Correo electrónico</label>
+        <input type="email" id="email" v-model="email" required
+          class="w-full p-2 border border-gray-300 rounded mt-1" />
+      </div>
+      <div class="mb-4">
+        <label for="message" class="block text-gray-700">Mensaje</label>
+        <textarea id="message" v-model="message" required
+          class="w-full p-2 border border-gray-300 rounded mt-1"></textarea>
+      </div>
+      <div class="g-recaptcha mb-4" data-sitekey="6LfWlX8qAAAAAPBA1xt4Lyqci8obXzNhMqziWybq"></div>
+      <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600">Enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+/*global grecaptcha*/
+export default {
+  name: 'ContactoView',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      recaptchaSiteKey: '6LfWlX8qAAAAAPBA1xt4Lyqci8obXzNhMqziWybq',
+      recaptchaLoaded: false,
+    };
+  },
+  methods: {
+    async submitForm() {
+      const recaptchaResponse = grecaptcha.getResponse();
+      if (!recaptchaResponse) {
+        alert('Por favor completar el CAPTCHA');
+        return;
+      }
+
+      const formData = {
+        nombre_completo: this.name,
+        correo_electronico: this.email,
+        mensaje: this.message,
+        recaptchaResponse: recaptchaResponse,
+      };
+
+      try {
+        const response = await axios.post('https://admin-grupo49.proyecto2024.linti.unlp.edu.ar/api/contacto', formData);
+        if (response.data.success) {
+          alert('Mensaje enviado correctamente');
+          this.name = '';
+          this.email = '';
+          this.message = '';
+          grecaptcha.reset();
+        } else {
+          alert('Fallo en la validación del CAPTCHA');
+        }
+      } catch (error) {
+        alert('Error enviando mensaje');
+      }
+    }
+
+
+  },
+  mounted() {
+    const script = document.createElement('script');
+    script.src = 'https://www.google.com/recaptcha/api.js';
+    script.async = true;
+    script.onload = () => {
+      this.recaptchaLoaded = true;
+    };
+    document.head.appendChild(script);
+  },
+};
+</script>
